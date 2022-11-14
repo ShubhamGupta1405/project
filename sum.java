42e4769da2deef6c507e34cff6319b4fa31c4e9b
Shubham Gupta <subhamguptarocks44@gmail.com>
2022-11-14 21:41:38 +0530

    commit2

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ class sum{
     public static void main (String arg[]){
         int a=30;
         int b=40;
-        System.out.println (a+b);
+        System.out.println ("The sum is: "+(a+b));
     }
 }
